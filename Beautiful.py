222d7649d18fd2b1c5de93a48d6ca65cf8182bbf
Rharmon32 <88513773+Rharmon32@users.noreply.github.com>
2021-08-23 00:48:06 -0500

    Add files via upload

---
+++ Beautiful.py
@@ -0,0 +1,15 @@
+name = "Priscilla"
+nick_name = "Beautiful"
+skin_color = "Lovely"
+
+yes_no = input('Do you like me? ')
+
+if yes_no == "yes":
+    print('What are you doing later? ', nick_name)
+
+else:
+    print('What don\'t you like about me?')
+
+       
+
+
